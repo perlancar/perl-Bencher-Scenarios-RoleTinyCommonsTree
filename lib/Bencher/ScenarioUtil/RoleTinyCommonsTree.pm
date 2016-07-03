ae65e4df453cb90478e4cada9dba6fd79773658d
perlancar (@pc-home) <perlancar@gmail.com>
2016-07-03 10:20:44 +0700

    Adjust to Tree-Object 0.03+ (Tree::Object::Hash)

--- lib/Bencher/ScenarioUtil/RoleTinyCommonsTree.pm
+++ lib/Bencher/ScenarioUtil/RoleTinyCommonsTree.pm
@@ -7,7 +7,7 @@ use strict;
 use warnings;
 
 use Data::Random::Tree qw(create_random_tree);
-use Tree::Object;
+use Tree::Object::Hash;
 
 use Exporter qw(import);
 our @EXPORT_OK = qw($tree_h3_o15 $tree_h4_o100 $tree_h6_o1k $tree_h7_o20k);
@@ -16,19 +16,19 @@ our %EXPORT_TAGS = (all => \@EXPORT_OK);
 # an example of a tiny tree
 our $tree_h3_o15 = create_random_tree(
     num_objects_per_level => [2, 4, 8],
-    classes => ['Tree::Object'],
+    classes => ['Tree::Object::Hash'],
 );
 
 # an example of a small tree
 our $tree_h4_o100 = create_random_tree(
     num_objects_per_level => [4, 16, 64, 15],
-    classes => ['Tree::Object'],
+    classes => ['Tree::Object::Hash'],
 );
 
 # an example of a small tree
 our $tree_h6_o1k = create_random_tree(
     num_objects_per_level => [10, 100, 600, 200, 99],
-    classes => ['Tree::Object'],
+    classes => ['Tree::Object::Hash'],
 );
 
 # this is a tree of height 7, ~20k objects. this is on par with my current
@@ -36,7 +36,7 @@ our $tree_h6_o1k = create_random_tree(
 # objects when parsed with Org::Parser.
 our $tree_h7_o20k = create_random_tree(
     num_objects_per_level => [100, 3000, 5000, 8000, 3000, 1000, 300],
-    classes => ['Tree::Object'],
+    classes => ['Tree::Object::Hash'],
 );
 
 1;
