9a7a7821488f65ad26f699fda74ed82ccfdb83ff
perlancar (on netbook-zenbook-ux305) <perlancar@gmail.com>
2017-01-25 15:26:30 +0700

    Release v0.04

--- lib/Bencher/Scenario/RoleTinyCommonsTree/Startup.pm
+++ lib/Bencher/Scenario/RoleTinyCommonsTree/Startup.pm
@@ -8,8 +8,8 @@ our $scenario = {
         'Role-TinyCommons-Tree distribution',
     module_startup => 1,
     modules => {
-        'Code::Includable::Tree::NodeMethods'  => {version=>0.06},
-        'Role::TinyCommons::Tree::NodeMethods' => {version=>0.06},
+        'Code::Includable::Tree::NodeMethods'  => {version=>0.11},
+        'Role::TinyCommons::Tree::NodeMethods' => {version=>0.11},
     },
     participants => [
         {module=>'Scalar::Util'},
